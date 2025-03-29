531b8c663083a4f0bccc5e8150ff5b90fac22648
vocados <vocados@gmail.com>
2025-03-30 00:01:04 +0900

    2025-03-30

---
+++ wp-content/plugins/kboard/rss.php
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?><rss version="2.0">
+	<channel>
+		<title>기차 시간표</title>
+		<link>https://train.vicharas.net/</link>
+		<description>KTX, ITX, 청춘, 열차에 대한 시간표를 제공합니다.</description>
+		
+			</channel>
+</rss>
